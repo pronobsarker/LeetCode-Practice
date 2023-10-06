0f0ffe952ae42ac9a4b83d11ccfc58b237b633a2
Pronob Sarker <pronobsarker@dhcp-214-5.cc.gettysburg.edu>
2023-10-06 01:12:15 -0400

    my commit

---
+++ Array & Hashing/GroupAnagram.java
@@ -0,0 +1,126 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/* 
+Strings are immutable in java.
+
+String original = "Hello";
+String modified = original.concat(", World!");  // Creates a new String object.
+
+System.out.println(original);   // Outputs: Hello
+System.out.println(modified);  // Outputs: Hello, World!
+
+In the above code, the original string remains "Hello", even after concatenation. The concat method returned a new string which is stored in modified.
+
+Arraylist offers constant-time performance for the basic operations (add, remove, get, and set), except for methods that involve shifting elements, such as add(index, element) or remove(index), which run in linear time.
+*/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+         //create and return the result list
+
+       List<List<String>> resultList = new ArrayList<>();
+
+
+       //we need a map from string --> list of strings
+
+
+       HashMap<String, List<String>> mp =new HashMap<>();
+
+
+       //loop through the string--> in java strings are immutable, so we need to create a char array
+
+
+       for(String str: strs) {
+           char [] tempArray = str.toCharArray(); //Converts this string to a new character array.
+           //sorted (aet) but in character
+           Arrays.sort(tempArray);
+
+
+           //create String version
+
+           String sorteString = new String(tempArray);
+
+
+           if (mp.containsKey(sorteString)) {
+               //add the original string
+               mp.get(sorteString).add(str);
+              
+           }
+           else {
+               List<String> tempList= new ArrayList<>();
+               resultList.add(tempList);
+               tempList.add(str);
+               mp.put(sorteString, tempList);     
+           }
+       }
+   //    for (Map.Entry<String, List<String>> entry : mp.entrySet()) {
+   //         resultList.add(entry.getValue());
+          
+   //     }
+       return resultList;
+      
+   }
+}
+
+
+    /**
+     * Algorithm:
+     * 1. loop through each string in the input.
+     * 2. sort the string.
+     * 3. insert it into the hashmap
+     * 4. iterate through the hasmap put each value in the result array.
+     * 5. return result array.
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * "eat","tea","tan","ate","nat","bat"
+     * 
+     * eat, tea
+     * after sort: aet
+     * Hashmap:
+     * key value(list)
+     * aet --> [eat, tea, ate]
+     * ant --> [tan, nat]
+     * abt --> [bat]
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * Convert the current string str to a character array (tempArray).
+     * Sort this character array. This is crucial because anagrams will have the
+     * same sorted sequence of characters.
+     * Convert the sorted character array back to a string (sorteString). This
+     * string acts as the key in the hashmap mp to group anagrams.
+     * Check if this sorted string already exists as a key in the hashmap.
+     * If it does, append the original string str to the list associated with this
+     * key.
+     * If it doesn't, create a new list, add the original string str to it, and then
+     * put this new list into the hashmap with sorteString as the key.
+     * example:
+     * 
+     * Let's take strs as ["eat", "tea", "tan"].
+     * 
+     * First Iteration (str = "eat"):
+     * 
+     * Convert "eat" to ['e', 'a', 't'].
+     * Sort to get ['a', 'e', 't'].
+     * Convert back to "aet".
+     * "aet" isn't in mp, so create a new list ["eat"] and add it to mp with "aet"
+     * as the key.
+     * 
+     */
+
+}
---
+++ Array & Hashing/ValidAnagram.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+
+/**
+ * ValidAnagram : words with exact same letters
+ */
+public class ValidAnagram {
+
+    // method to make the freq map
+    static HashMap<Character, Integer> makeFreqMap(String str) {
+
+        // it creates a hashmap
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        // traverse the string and add key-value pair in the hashmap
+
+        for (int i = 0; i < str.length(); i++) {
+            // pick the char and check if it has been added before
+
+            Character ch = str.charAt(i);
+            // if the char isn't added before
+            if (!mp.containsKey(ch)) {
+
+                mp.put(ch, 1);
+            } else {
+                int currFeq = mp.get(ch);
+                mp.put(ch, currFeq + 1);
+
+            }
+
+        }
+        return mp;
+
+    }
+
+    public boolean isAnagram(String s, String t) {
+
+        // if the the strings have different length then return false
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // call the method to make the freq map of string s and t
+        HashMap<Character, Integer> mp1 = makeFreqMap(s); // why not new here?
+        HashMap<Character, Integer> mp2 = makeFreqMap(t);
+
+        // if the freqmap are equal return true
+        return (mp1.equals(mp2));
+    }
+
+}
